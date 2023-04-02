891ea24dd08caf8ddf72ce8e1287e09dd13a5f15
maxxxxxxy <cvb.cvb.2017.cvb@gmail.com>
2023-04-02 15:58:39 +0300

    init commit

---
+++ src/main/java/com/example/fynotify/FynotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fynotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FynotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FynotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fynotify/FynotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fynotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FynotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
